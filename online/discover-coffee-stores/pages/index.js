5bceb35c8bd6067f864c37326c8fedfa12c6d8ec
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-02 16:00:24 +0900

    [STUDY] online-tutirial

---
+++ online/discover-coffee-stores/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import Banner from '../components/banner/banner';
+
+export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log('Hi banner');
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Coffe Connoissueur</h1>
+        <Banner
+          buttonText='View stores nearby'
+          handleOnClick={handleOnBannerBtnClick}
+        />
+      </main>
+
+      {/* <footer className={styles.footer}></footer> */}
+    </div>
+  );
+}
